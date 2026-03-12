12757d4112180afb9b8e6e208c9731bcea4dee8c
Artyom Abramov <geroshold@hotmail.com>
2026-03-13 00:28:32 +0600

    Скрипт игры
    Добавлен скрипт для игры, можно перетаскивать вилки и подключать их
    TODO: 1. Сделать, чтобы тянулись провода; 2. Добавить спрайт для подключенной вилки и переключать его в коде

--- scenes/levels/WiresGame/wire.gd
+++ scenes/levels/WiresGame/wire.gd
@@ -5,13 +5,15 @@ extends Area2D
 
 @onready var plug_corpus = $PlugCorpus
 @onready var line = $Wire/Line
+@onready var touch_area = $TouchArea
 @onready var start_position = global_position
 
 signal plug_connected
 
 var is_dragging = false
 var is_connected = false
-var mouse_offset = Vector2()
+var drag_offset = Vector2()
+var touch_index = -1
 
 func _ready():
 	match color_type:
@@ -31,22 +33,33 @@ func _ready():
 	line.width = 10
 	_update_line()
 
-func _input_event(viewport, event, shape_idx):
+func _input(event):
 	if is_connected:
 		return
 	
 	if event is InputEventScreenTouch:
 		if event.pressed:
-			mouse_offset = global_position - get_global_mouse_position()
-			is_dragging = true
+			if _is_point_inside_plug(event.position):
+				touch_index = event.index
+				drag_offset = global_position - event.position
+				is_dragging = true
+				print("Начало перетаскивания")
 		else:
-			is_dragging = false
-			_try_connect()
+			if event.index == touch_index:
+				is_dragging = false
+				touch_index = -1
+				_try_connect()
+	
+	if event is InputEventScreenDrag:
+		if event.index == touch_index and is_dragging:
+			global_position = event.position + drag_offset
+			_update_line()
 
-func _process(delta):
-	if is_dragging:
-		global_position = get_global_mouse_position() + mouse_offset
-		_update_line()
+func _is_point_inside_plug(point):
+	var local_point = to_local(point)
+	var circle_shape = touch_area.shape as CircleShape2D
+	var distance = local_point.length()
+	return distance <= circle_shape.radius
 
 func _try_connect():
 	var overlapping_areas = get_overlapping_areas()
