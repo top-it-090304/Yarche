a73c88f655bd9fafcd5d38db2fd392a75445ffab
Artyom Abramov <geroshold@hotmail.com>
2026-03-12 23:17:39 +0600

    Изменения в скрипте розетки
    В скрипте розетки добавлено окрашивание спрайта в зависимости от указанного цвета

--- scenes/objects/WiresGame/socket.gd
+++ scenes/objects/WiresGame/socket.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
 @export var color_type = "red"
+@onready var socket = $Socket
 
 func _ready():
 	add_to_group("sockets")
+	match color_type:
+		"red":
+			socket.modulate = Color.RED
+		"blue":
+			socket.modulate = Color.BLUE
+		"green":
+			socket.modulate = Color.GREEN
+		"yellow":
+			socket.modulate = Color.YELLOW
