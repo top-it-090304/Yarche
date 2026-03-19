723738ac230182200a6d4a5b723da3d8a9d62d4e
Artyom Abramov <geroshold@hotmail.com>
2026-03-19 21:29:49 +0600

    Revert "Таймер в игре про насос"
    This reverts commit 3f5884286bffcf224a34ead9a17690e31a0ee98c.

--- scenes/levels/WiresGame/game.gd
+++ scenes/levels/WiresGame/game.gd
@@ -16,6 +16,7 @@ func _ready():
 	timer_bar.max_value = game_time
 	timer_bar.value = game_time
 	
+	# Находим все вилки и подключаем их сигналы
 	var plugs = get_tree().get_nodes_in_group("plugs")
 	print("Найдено вилок: ", plugs.size())
 	for plug in plugs:
