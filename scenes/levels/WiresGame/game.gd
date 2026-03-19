404e808ed5a4132c6eca6ec074983aa347b05c08
Artyom Abramov <geroshold@hotmail.com>
2026-03-19 21:47:04 +0600

    Стандартизация игры про провода
    Игра с проводами приведена к новой концепции, добавлено экспортное время и сигналы победы/поражения

--- scenes/levels/WiresGame/game.gd
+++ scenes/levels/WiresGame/game.gd
@@ -1,34 +1,31 @@
 extends Node2D
 
-@onready var timer_bar = $"../TimerBar"
 @onready var game_timer = $GameTimer
-var game_time = 10
 var game_active = true
+
+@export var timer_time: float
+signal win
+signal lose
 
 var connected_plugs_count = 0
 
 func _ready():
-	game_timer.wait_time = game_time
+	game_timer.wait_time = timer_time
 	game_timer.one_shot = true
 	game_timer.timeout.connect(_on_game_timer_timeout)
 	game_timer.start()
 	
-	timer_bar.max_value = game_time
-	timer_bar.value = game_time
-	
-	# Находим все вилки и подключаем их сигналы
 	var plugs = get_tree().get_nodes_in_group("plugs")
-	print("Найдено вилок: ", plugs.size())
 	for plug in plugs:
 		plug.plug_connected.connect(_on_plug_connected)
 
 func _process(delta):
-	timer_bar.value = game_timer.time_left
+	pass
 
 func _on_plug_connected():
 	connected_plugs_count += 1
 	if connected_plugs_count == 4:
-		pass
+		win.emit()
 	
 func _on_game_timer_timeout():
-	pass
+	lose.emit()
