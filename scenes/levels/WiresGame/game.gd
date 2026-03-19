ecaf0726422d0469b1d266f877a3a6fc9627f950
Artyom Abramov <geroshold@hotmail.com>
2026-03-19 21:49:58 +0600

    Обучение в игре про провода
    В игру про провода добавлен показ обучения (пока со старой анимацией)

--- scenes/levels/WiresGame/game.gd
+++ scenes/levels/WiresGame/game.gd
@@ -10,6 +10,9 @@ signal lose
 var connected_plugs_count = 0
 
 func _ready():
+	get_tree().paused = true
+	show_tutorial()
+	
 	game_timer.wait_time = timer_time
 	game_timer.one_shot = true
 	game_timer.timeout.connect(_on_game_timer_timeout)
@@ -18,6 +21,14 @@ func _ready():
 	var plugs = get_tree().get_nodes_in_group("plugs")
 	for plug in plugs:
 		plug.plug_connected.connect(_on_plug_connected)
+
+func show_tutorial():
+	var tutorial = preload("res://scenes/levels/PumpGame/pump_tutorial.tscn").instantiate()
+	add_child(tutorial)
+	tutorial.tutorial_finished.connect(_start_game)
+
+func _start_game():
+	get_tree().paused = false
 
 func _process(delta):
 	pass
