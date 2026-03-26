69354363846792708cf3faf6810fd94f1b06eed0
Pavel <pasiforg@gmail.com>
2026-03-27 00:06:04 +0600

    Добавлен туториал на управление в игру, перенесена в базоый уровень

--- scenes/levels/AvoidTheGarbage/avoid_the_garbage.gd
+++ scenes/levels/AvoidTheGarbage/avoid_the_garbage.gd
@@ -2,11 +2,13 @@ extends Node2D
 @onready var fish: CharacterBody2D = $gameworld/Control/Fish
 @onready var timer: Timer = $Timer
 @export var timer_time: float = 2
-
+var tutorial_scene = preload("res://scenes/levels/AvoidTheGarbage/move_tutorial.tscn")
 signal win
 signal lose
 
 func _ready():
+	get_tree().paused = true
+	show_tutorial()
 	timer.wait_time = timer_time
 	
 	timer.timeout.connect(_win_ending)
@@ -14,9 +16,17 @@ func _ready():
 	
 	timer.start()
 	
+func _start_game():
+	get_tree().paused = false
+	
 func _win_ending():
 	win.emit()
 
 func _lose_ending():
 	print("бамп")
 	lose.emit()
+	
+func show_tutorial():
+	var tutorial = tutorial_scene.instantiate()
+	add_child(tutorial)
+	tutorial.tutorial_finished.connect(_start_game)
