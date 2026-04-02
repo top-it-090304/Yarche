cf35b351be3fa915fb8b2e8dd00b3b524745f1d7
Pavel <pasiforg@gmail.com>
2026-04-02 21:21:10 +0600

    Добавлена адаптив туториала игры про бобров

---
+++ scripts/tutorial.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+@onready var timer = $Timer
+@onready var sprite: AnimatedSprite2D = $Control/Control/AnimatedSprite2D
+
+signal tutorial_finished
+
+func _ready():
+	sprite.play()
+	
+	timer.wait_time = 3.0
+	timer.one_shot = true
+	timer.timeout.connect(_on_tutorial_timeout)
+	timer.start()
+
+func _on_tutorial_timeout():
+	emit_signal("tutorial_finished")
+	queue_free()
+
+func skip_tutorial():
+	timer.stop()
+	_on_tutorial_timeout()
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		skip_tutorial()
