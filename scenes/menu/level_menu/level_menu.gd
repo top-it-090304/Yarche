c6e184c29566b47cb9674b79134dfcfd4bac7122
Pavel <pasiforg@gmail.com>
2026-04-02 23:35:19 +0600

    Улучшение разрешения кнопок уровней, их стилизация, улучшение анимации появления меню уровней, адаптив менню уровней под все экраны

--- scenes/menu/level_menu/level_menu.gd
+++ scenes/menu/level_menu/level_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 @onready var grid_container: GridContainer = $GridContainer
 const LEVEL_BUTTON = preload("res://scenes/menu/level_menu/LevelButton.tscn")
-signal rendered
+
 func _ready():
 	for i in range(1,15):
 		var button = LEVEL_BUTTON.instantiate()
--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
 const LEVEL_MENU = preload("res://scenes/menu/level_menu/level_menu.tscn")
+
 @onready var main_menu_ui: Control = $MainMenuUi
 var flag = false
+
 func _ready():
 	main_menu_ui.play_button_pressed.connect(_show_level_menu)
 	
@@ -18,13 +20,13 @@ func _close_main_menu():
 	main_menu_ui.position = Vector2(0,0)
 	var tween_close_main_menu_ui = create_tween().set_parallel(true)
 	tween_close_main_menu_ui.tween_property(main_menu_ui, "modulate", Color(1,1,1,0), 0.5)
-	tween_close_main_menu_ui.tween_property(main_menu_ui, "position", Vector2(0, 720), 0.5)
+	tween_close_main_menu_ui.tween_property(main_menu_ui, "position", Vector2(0, 720), 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	tween_close_main_menu_ui.tween_callback(_delete_ui).set_delay(0.5)
 
 func _show_level_menu_with_dissolve(level_menu):
 	var tween_show_level_menu = create_tween().set_parallel(true)
 	tween_show_level_menu.tween_property(level_menu, "modulate", Color(1,1,1,1), 0.5)
-	tween_show_level_menu.tween_property(level_menu, "position", Vector2(0,0), 0.5)
+	tween_show_level_menu.tween_property(level_menu, "position", Vector2(0,0), 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	
 func _delete_ui():
 	main_menu_ui.queue_free()
