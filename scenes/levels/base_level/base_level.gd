429b56deaaa827d28c1ec60dbb1f5bfe5ff40e5d
Pavel <pasiforg@gmail.com>
2026-04-02 20:34:48 +0600

    Удален аддон на скелетную анимацию, мини-игры, которые его использовали временно недоступны (вырезаны из уровня, но файлы сцен остались). p.s. Экспорт под аврору не поддерживает использование GD Extensions, ввиду этой причины упрощаем внешний вид

--- scenes/levels/base_level/base_level.gd
+++ scenes/levels/base_level/base_level.gd
@@ -6,9 +6,7 @@ var win_scene_preload = preload("res://scenes/menu/win_scene/win-scene.tscn")
 var lose_scene_preload = preload("res://scenes/menu/lose_scene/LoseScene.tscn")
 var times = [10, 7, 5, 5,10,12]
 var game_paths = [
-	"res://scenes/levels/AvoidTheGarbage/AvoidTheGarbage.tscn",
 	"res://scenes/levels/HitTheBeaverGame/HitTheBeaverGame.tscn",
-	"res://scenes/levels/TapGame/tap_game.tscn",
 	"res://scenes/levels/PumpGame/pump_game.tscn",
 	"res://scenes/levels/FireGame/FireGame.tscn", 
 	"res://scenes/levels/WiresGame/wires_game.tscn",
