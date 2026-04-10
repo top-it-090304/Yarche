970709ad8ed277a3ed68ba70c6f472c9fe546fa1
Pavel Zhivushko <pasiforg@gmail.com>
2026-04-10 16:43:16 +0600

    ТУториал новой мини-игры, увеличен оттуп текста от верхней границы во всх туториалах.

--- scenes/levels/CleanGame/clean_game.gd
+++ scenes/levels/CleanGame/clean_game.gd
@@ -7,11 +7,21 @@ signal lose
 @export var timer_time: float = 4.0
 
 func _ready():
+	show_tutorial()
 	timer.wait_time = timer_time
 	timer.timeout.connect(_lose)
 	banana.come_back.connect(_win)
 	
 	timer.start()
+
+func show_tutorial():
+	get_tree().paused = true
+	var tutorial = preload("res://scenes/levels/CleanGame/Clean_tutorial.tscn").instantiate()
+	add_child(tutorial)
+	tutorial.tutorial_finished.connect(_start_game)
+
+func _start_game():
+	get_tree().paused = false
 
 func _win():
 	win.emit()
