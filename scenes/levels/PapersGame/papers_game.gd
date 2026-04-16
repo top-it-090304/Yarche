1f34f07da3e66f393760870bd1cab442b38f8a65
Pavel <pasiforg@gmail.com>
2026-04-17 00:44:30 +0600

    Туториал обучения новой мини-игры

--- scenes/levels/PapersGame/papers_game.gd
+++ scenes/levels/PapersGame/papers_game.gd
@@ -2,17 +2,23 @@ extends Node2D
 @export var timer_time: float
 @onready var timer: Timer = $GameTimer
 @onready var game_world: Node = $game_world
-
+var tutorial_scene = preload("res://scenes/levels/PapersGame/PaperTutorial.tscn")
 signal win
 signal lose
 
 func _ready():
+	show_tutorial()
 	timer.wait_time = timer_time
 	game_world.all_checked.connect(_win)
 	timer.timeout.connect(_lose)
 	timer.start()
 
-
+func show_tutorial():
+	get_tree().paused = true
+	var tutorial = tutorial_scene.instantiate()
+	tutorial.tutorial_finished.connect(_start_game)
+	add_child(tutorial)
+	
 func _win():
 	win.emit()
 	
