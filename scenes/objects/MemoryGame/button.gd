6d6c7040feb1430b9d3dea968cd2a83155af89fc
Artyom Abramov <geroshold@hotmail.com>
2026-04-17 02:08:35 +0600

    Исправление кнопки
    Кнопки переделаны под sprite2d с collision shape, вместо button. У спрайтов сильнее затемнена нижняя область

--- scenes/objects/MemoryGame/button.gd
+++ scenes/objects/MemoryGame/button.gd
@@ -1,21 +1,66 @@
-extends Button
+extends Area2D
 class_name GameButton
 
 @export var button_id = ""
 @export var button_color = Color.WHITE
-@export var shape_texture : Texture2D
+@export var default_texture: Texture2D  
+
+@onready var sprite_2d = $Sprite2D
 
 signal on_pressed(button: GameButton)
 
+var is_interactable = true
+var original_scale: Vector2
+var tween: Tween
+
 func _ready():
-	modulate = button_color
-	if shape_texture:
-		icon = shape_texture
-		expand_icon = true
-		icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	pressed.connect(func(): on_pressed.emit(self))
+	original_scale = scale
+	
+	if default_texture:
+		sprite_2d.texture = default_texture
+	
+	sprite_2d.modulate = button_color
+	
+	input_event.connect(_on_input_event)
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if not is_interactable:
+		return
+	
+	if event is InputEventScreenTouch and event.pressed:
+		on_touch()
+	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		on_touch()
+
+func on_touch():
+	animate_press()
+	
+	on_pressed.emit(self)
+
+func animate_press():
+	if tween:
+		tween.kill()
+	
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_BACK)
+	
+	tween.tween_property(self, "scale", original_scale * 0.85, 0.08)
+	tween.tween_property(self, "scale", original_scale, 0.12)
+
+func set_interactable(value):
+	is_interactable = value
+	$CollisionShape2D.disabled = not value
+	
+	if not value:
+		sprite_2d.modulate.a = 0.5
+	else:
+		sprite_2d.modulate.a = 1.0
 
 func flash():
-	var tween = create_tween()
-	tween.tween_property(self, "modulate", Color.WHITE, 0.1)
-	tween.tween_property(self, "modulate", button_color, 0.1)
+	if tween:
+		tween.kill()
+	
+	tween = create_tween()
+	tween.tween_property(sprite_2d, "modulate", Color.WHITE, 0.1)
+	tween.tween_property(sprite_2d, "modulate", button_color, 0.1)
