722ae4ba8cbe29ed20260e24df217dcffd8a9d88
Pavel <pasiforg@gmail.com>
2026-04-20 21:46:41 +0600

    Анимация падения очистка сделана красивее

--- scenes/objects/PotatoGame/Potato/potato.gd
+++ scenes/objects/PotatoGame/Potato/potato.gd
@@ -9,9 +9,12 @@ var peels
 func slice():
 	if peels.size() > 0:
 		var peel = peels.pop_at(0)
-		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
-		tween.tween_property(peel, "position:y", 1080+500, 1.0)
-		tween.tween_callback(check_state)
+		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART).set_parallel(true)
+		tween.tween_property(peel, "global_position:y", 1080+500, 1)
+		tween.tween_property(peel, "rotation", deg_to_rad(15), 1)
+		tween.tween_property(peel, "scale", peel.scale*1.1, 1)
+		await get_tree().create_timer(1).timeout
+		check_state()
 		
 func check_state():
 	if peels.size() == 0:
