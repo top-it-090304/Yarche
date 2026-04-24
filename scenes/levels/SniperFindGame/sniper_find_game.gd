102ab517ae33d3374538cf444b3c0658e1b6cedc
Artyom Abramov <geroshold@hotmail.com>
2026-04-24 16:29:03 +0600

    Черная пелена
    В игре про нахождение снайпера добавлен CanvasModulate, который закрывает весь экран черным и прорезается светом от прицела

--- scenes/levels/SniperFindGame/sniper_find_game.gd
+++ scenes/levels/SniperFindGame/sniper_find_game.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-@onready var crosshair = $Crosshair
+@onready var crosshair = $CanvasLayer/Crosshair
 @onready var sniper = $Sniper
 
-
+func _process(_delta):
+	crosshair.global_position = get_global_mouse_position()
+	
 func _ready():
 	pass
