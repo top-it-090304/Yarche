b0c52cf73757a2ec2e41c51bea91e198a89a6167
Pavel <pasiforg@gmail.com>
2026-04-29 19:02:33 +0600

    Исключен выбор уровня

--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -1,32 +1,20 @@
 extends CanvasLayer
 const LEVEL_MENU = preload("res://scenes/menu/level_menu/level_menu.tscn")
-
+const animation_time = 1
+@onready var overlay: ColorRect = $overlay
 @onready var main_menu_ui: Control = $MainMenuUi
-var flag = false
 
 func _ready():
-	main_menu_ui.play_button_pressed.connect(_show_level_menu)
+	main_menu_ui.play_button_pressed.connect(start_game)
 	
-func _show_level_menu():
-	var level_menu = LEVEL_MENU.instantiate()
-	level_menu.position = Vector2(0, -get_viewport().get_visible_rect().size.y)
-	level_menu.modulate = Color(1,1,1,0)
-	add_child(level_menu)
-	
+func start_game():
 	_close_main_menu()
-	_show_level_menu_with_dissolve(level_menu)
-
+	await get_tree().create_timer(animation_time+1).timeout
+	get_tree().change_scene_to_file("res://scenes/levels/base_level/base_level.tscn")
+	
 func _close_main_menu():
 	main_menu_ui.position = Vector2(0,0)
 	var tween_close_main_menu_ui = create_tween().set_parallel(true)
-	tween_close_main_menu_ui.tween_property(main_menu_ui, "modulate", Color(1,1,1,0), 0.5)
-	tween_close_main_menu_ui.tween_property(main_menu_ui, "position", Vector2(0, 720), 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	tween_close_main_menu_ui.tween_callback(_delete_ui).set_delay(0.5)
-
-func _show_level_menu_with_dissolve(level_menu):
-	var tween_show_level_menu = create_tween().set_parallel(true)
-	tween_show_level_menu.tween_property(level_menu, "modulate", Color(1,1,1,1), 0.5)
-	tween_show_level_menu.tween_property(level_menu, "position", Vector2(0,0), 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	
-func _delete_ui():
-	main_menu_ui.queue_free()
+	tween_close_main_menu_ui.tween_property(main_menu_ui, "modulate", Color(1,1,1,0), animation_time)
+	tween_close_main_menu_ui.tween_property(main_menu_ui, "position", Vector2(0, 1080/1.5), animation_time).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween_close_main_menu_ui.tween_property(overlay, "modulate", Color(1,1,1,1), animation_time)
