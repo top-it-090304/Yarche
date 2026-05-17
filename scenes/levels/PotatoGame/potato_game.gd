35c2483e9a7f28d1c9d6425cd5546d7e79c08cfe
Pavel Zhivushko <pasiforg@gmail.com>
2026-05-17 17:04:27 +0600

    Туториал игры про картошку

--- scenes/levels/PotatoGame/potato_game.gd
+++ scenes/levels/PotatoGame/potato_game.gd
@@ -7,17 +7,19 @@ signal lose
 @export var timer_time: float = 4.0
 
 func _ready():
+	show_tutorial()
+
 	timer.wait_time = timer_time
 	timer.timeout.connect(_lose)
 	potato.is_clean.connect(_win)
 	
 	timer.start()
 
-#func show_tutorial():
-	#get_tree().paused = true
-	#var tutorial = preload("res://scenes/levels/CleanGame/Clean_tutorial.tscn").instantiate()
-	#add_child(tutorial)
-	#tutorial.tutorial_finished.connect(_start_game)
+func show_tutorial():
+	get_tree().paused = true
+	var tutorial = preload("res://scenes/levels/PotatoGame/PotatoTutorial.tscn").instantiate()
+	add_child(tutorial)
+	tutorial.tutorial_finished.connect(_start_game)
 
 func _start_game():
 	get_tree().paused = false
