3ea3be71fcecacaad77920f7dc6becc2f5825f5e
zhivushko <pasiforg@gmail.com>
2026-05-17 15:26:12 +0600

    Настройка камеры во всех мини-играх, унификация

--- scenes/levels/base_level/base_level.gd
+++ scenes/levels/base_level/base_level.gd
@@ -36,7 +36,7 @@ func _ready():
 func set_level_data():
 	mini_games.shuffle()
 	
-	level_data = [{"time": 1, "path":"res://scenes/levels/TapGame/tap_game.tscn"}] + mini_games.slice(0,5)
+	level_data = mini_games.slice(0,5)
 	
 func _set_timer_bar(time):
 		
